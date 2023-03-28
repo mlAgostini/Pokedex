cb490c5a836985736a550db31250bfc336e70084
mlAgostini <valmont.vakarian@gmail.com>
2023-03-28 16:45:32 +0200

    Ajout du premier props

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -3,11 +3,27 @@ import PokemonCard from './components/PokemonCard';
 
 function App() {
 
+  const pokemon = pokemonList[0];
   return (
     <div>
-      <PokemonCard/>
+      <PokemonCard
+      name={pokemon.name}
+      imgSrc={pokemon.imgSrc}
+      />
     </div>
   );
 }
+
+const pokemonList = [
+  {
+      name: "bulbasaur",
+      imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+
+  {
+      name: "mew",
+  },
+];
 
 export default App;
--- pokedex/src/components/PokemonCard.jsx
+++ pokedex/src/components/PokemonCard.jsx
@@ -1,9 +1,9 @@
-function PokemonCard() {
-    const pokemon = pokemonList[0];
+function PokemonCard(props) {
+    
     return (
         <figure>
-            {pokemon.imgSrc ? (
-            <img src={pokemon.imgSrc} alt={pokemon.name}/>
+            {props.imgSrc ? (
+            <img src={props.imgSrc} alt={props.name}/>
             ) : (
                 <p>???</p>
             )}
@@ -12,16 +12,6 @@ function PokemonCard() {
     );
 }
 
-const pokemonList = [
-    {
-        name: "bulbasaur",
-        imgSrc:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
 
-    {
-        name: "mew",
-    },
-];
 
 export default PokemonCard;
